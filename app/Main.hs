ed9735df1ad79ebbca87a3ff702d7c51f969fc64
rayankermanshahani <rayan.k@gmx.com>
2025-03-25 10:31:16 -0400

    boilerplate for command line argument parsing

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,9 @@
 module Main (main) where
 
+import qualified Data.Vector.Unboxed as VU
+import Options.Applicative
+import System.IO
+
 import NTSC.Audio
 import NTSC.Color
 import NTSC.Frame
@@ -7,5 +11,40 @@ import NTSC.Signal
 import NTSC.Sync
 import NTSC.Types
 
+-- command line argument parsing and main entry point
+datga Args = Args 
+  { inputFile :: FilePath,
+    outputFile :: FilePath
+  }
+
 main :: IO ()
-main = putStrLn "main program entry point"
+main = do
+  args <- parseArgs
+  demodulateNTSC (inputFile args) (outputFile args)
+
+demodulateNTSC :: FilePath -> FilePath -> IO ()
+demodulateNTSC input output = do
+  -- the full signal processing pipeline goes here:
+  -- reading -> sync detection -> color processing -> output
+  error "TODO"
+
+parseArgs :: IO Args
+parseArgs = execParser opts
+  where
+    opts = info (argsParser <**> helper)
+      ( fullDesc
+     <> progDesc "NTSC Signal Demodulator"
+     <> header "ntsc-demodulator - convert NTSC signals to decoded video/audio" )
+
+argsParser :: Parser Args
+argsParser = argsParser
+  <$> strOption
+      ( long "input"
+     <> short 'i'
+     <> metavar "FILE"
+     <> help "Input NTSC signal file" )
+  <$> strOption
+      ( long "output"
+     <> short 'o'
+     <> metavar "FILE"
+     <> help "Output file for demodulated content" )
